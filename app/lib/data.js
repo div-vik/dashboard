05152eaa52f3008b870c17e563ec274f6a27c30d
div-vik <shivamambast5@gmail.com>
2023-12-28 22:01:29 +0530

    Create fn to fetch users

---
+++ app/lib/data.js
@@ -0,0 +1,14 @@
+import { User } from "./models";
+import { connectDB } from "./utils";
+
+export const fetchUsers = async (q) => {
+  const regex = new RegExp(q, "i");
+  try {
+    connectDB();
+    const users = await User.find({ username: { $regex: regex } });
+    return users;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to fetch users!");
+  }
+};
